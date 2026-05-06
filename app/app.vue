9cf4e90946f3bfdeebc60d7f59cf99e96c7badfb
szuryuu <ilhamdzaky2007@gmail.com>
2026-05-07 00:34:20 +0700

    fix: state dark mode/light mode

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+useHead({
+  script: [
+    {
+      innerHTML: `
+        if (localStorage.getItem('theme-preference') === 'dark' || (!('theme-preference' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+        }
+      `,
+      type: "text/javascript",
+    },
+  ],
+});
+</script>
 
 <template>
   <NuxtLayout>
