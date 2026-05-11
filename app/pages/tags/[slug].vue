1ad270a66ffbeeeea955467c7341822a349d2bb6
szuryuu <ilhamdzaky2007@gmail.com>
2026-05-11 19:01:21 +0700

    fix: dynamic route reactivity

---
+++ app/pages/tags/[slug].vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const tag = computed(() => String(route.params.slug || ""));
+</script>
+
+<template>
+  <ListLayout
+    :title="`#${tag.toUpperCase()}`"
+    :collection="['writing', 'ctf', 'infrastructure', 'projects']"
+    :tag-filter="tag"
+    empty-message="No transmissions found for this tag."
+  />
+</template>
